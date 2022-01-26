500fb039f73d41c48bf294be7fa3337124f03f38
Fangting Ma <64255604+Erica233@users.noreply.github.com>
2022-01-26 16:45:23 -0500

    Create test_main.py

---
+++ test_main.py
@@ -0,0 +1,13 @@
+from main import add, sub, mul, div
+
+def test_add():
+    assert add(3, 1) == {"total": 4}
+   
+def test_sub():
+    assert sub(3, 1) == {"total": 2}
+    
+def test_mul():
+    assert mul(3, 2) == {"total": 6}
+    
+def test_div():
+    assert div(4, 2) == {"total": 2}
